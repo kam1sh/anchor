5787da192f4ec4afb0c463fddac331fee572c737
Igor Ovsyannikov <kamish@outlook.com>
2019-04-19 16:23:19 +0300

    various setting fixes
    * Fixed timezone name
    * Removed docker environment checks
    * Added database fallback to sqlite if no postgres available
    * Added django applications in pyproject.toml

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC+03:00"
+TIME_ZONE = "Europe/Moscow"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -39,7 +39,7 @@ USE_TZ = True
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL", "sqlite:db.sqlite3")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 # URLS
