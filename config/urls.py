ece1c382a798ea1b54c5a805be99226efad55f80
Igor Ovsyannikov <kamish@outlook.com>
2019-04-29 13:25:11 +0300

    Code style
    * ran black over all project
    * removed uneccessary tags in html

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,8 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("ciconia.users.urls", namespace="users")),
-
     # Your stuff: custom urls includes go here
-    path("py/", include("ciconia.pypi.urls"))
+    path("py/", include("ciconia.pypi.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
